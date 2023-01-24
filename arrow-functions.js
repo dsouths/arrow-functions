15e69637056fd709d445d40656a44e3214e4ddb4
dsouths <dsouths@hotmail.com>
2023-01-24 21:15:50 +0000

    added answers

--- arrow-functions.js
+++ arrow-functions.js
@@ -8,13 +8,29 @@
 function addTwoNumbers(a,b) {
     return a+b;
 }
-let sum = addTwoNumbers(3,5);
-console.log(sum);
+//let sum = addTwoNumbers(3,5);
+//console.log(sum);
 
 // Arrow Function With Parameters
 
+addTwoNumbers = (a,b) => {
+    return a+b
+}
+let sum = addTwoNumbers(3,5);
+console.log(sum)
+
 // Single Line Arrow Function With Parameters
 
+const addTwoNumbers2 = (a,b) => a+b;
+let sum2 = (3+5) 
+console.log(sum2)
+
 // Implicit Returns
+
+const saySomething = message => console.log(message);
+saySomething("Hello Worldy")
+
+const sayHello = () => console.log("Hello");
+sayHello();
 
 // Returning Multiple Lines
